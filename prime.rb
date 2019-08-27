6b657331c4c688d1d1a713619fcbf992c2f85aa9
Stef Spears <spears.sj87@gmail.com>
2019-08-27 18:33:59 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1 
+    return false 
+    elsif (2..Math.sqrt(num).floor).any? {|i| num % i == 0}
+    return false 
+  else return true 
+    end
+end
