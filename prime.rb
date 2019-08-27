58bbbfd56fac9ac5fe389443955481707ad7a054
Stef Spears <spears.sj87@gmail.com>
2019-08-27 18:32:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(num)
   if num <= 1 
     return false 
     elsif (2..Math.sqrt(num).floor).any? {|i| num % i == 0}
-    return false
+    return false 
+  else return true 
+    end
   end
-  
-  
 end
